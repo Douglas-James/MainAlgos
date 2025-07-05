c98e30811afea9b67f040b6276303190d38c5e65
Douglas_James <104114920+Douglas-James@users.noreply.github.com>
2025-07-04 21:59:39 -0500

    Fix merge sort implementation by adding missing merge function import

--- Algorithms/section-2/Sort/mergeSort.js
+++ Algorithms/section-2/Sort/mergeSort.js
@@ -6,6 +6,8 @@ This is an implementation of the Merge Sort algorithm.
 It sorts an array by dividing it into halves, sorting each half, and then merging the sorted halves back together.
 =======================
 */
+const merge = require("./mergeArrays");
+
 // recrusive merge sort
 const mergeSort = (arr) => {
   if (arr.length <= 1) return arr;
